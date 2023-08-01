d5233aa9e179c5acbab393631a0056df33cf1003
barSUCHKA <139664450+barSUCHKA@users.noreply.github.com>
2023-08-01 18:12:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-let year = document.querySelector("#years");
-let month = document.querySelector("#monthes");
-let day = document.querySelector("#days");
-let hour = document.querySelector("#hours");
-let minute = document.querySelector("#minutes");
-let second = document.querySelector("#seconds");
+const time = document.querySelector("#time");
 
 function clock() {
     let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth();
+    let day = date.getDay();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
 
-    year.textContent = date.getFullYear();
-    month.textContent = date.getMonth();
-    day.textContent = date.getDay();
-    hour.textContent = date.getHours();
-    minute.textContent = date.getMinutes();
-    second.textContent = date.getSeconds();
-    if (month.textContent < 10) {
-        month.textContent = "0" + date.getMonth();
+    if (month < 10) {
+        month = "0" + date.getMonth();
     }
-    if (day.textContent < 10) {
-        day.textContent = "0" + date.getDay();
+    if (day < 10) {
+        day = "0" + date.getDay();
     }
-    if (hour.textContent < 10) {
-        hour.textContent = "0" + date.getHours();
+    if (hour < 10) {
+        hour = "0" + date.getHours();
     }
-    if (minute.textContent < 10) {
-        minute.textContent = "0" + date.getMinutes();
+    if (minute < 10) {
+        minute = "0" + date.getMinutes();
     }
-    if (second.textContent < 10) {
-        second.textContent = "0" + date.getSeconds();
+    if (second < 10) {
+        second = "0" + date.getSeconds();
     }
+
     time.insertAdjacentHTML("beforeend", `
     <span id="years">${year}</span>-<span id="monthes">${month}</span>-<span id="days">${day}</span>-<span id="hours">${hour}</span>-<span id="minutes">${minute}</span>-<span id="seconds">${second}</span>
     `);
+
+
 }
 
 clock();
